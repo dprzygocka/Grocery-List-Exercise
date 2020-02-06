ef251a74615bf5262c5db9c844c5ef0a0ef28342
Dagmara Przygocka <przygocka.dagmara@gmail.com>
2020-02-06 15:02:45 +0100

    groceriesgit add src/GroceryMain.java !

--- src/GroceryMain.java
+++ src/GroceryMain.java
@@ -13,7 +13,6 @@ public class GroceryMain{
         new GroceryItemOrder("beer", 6, 20, list);
         new GroceryItemOrder("spinach", 1, 15.99, list);
         new GroceryItemOrder("cheese", 2, 23.99, list);
-        new GroceryItemOrder("beer", 6, 20, list);
         new GroceryItemOrder("spinach", 1, 15.20, list);
 
         System.out.println("This is the grocery list:");
