1c81b3cce6eb638448c7ee9018f12d5d2aee41a8
Barbara Rekowska <basnow28@gmail.com>
2020-02-07 08:59:18 +0100

    File Handler - loading groceries list from txt file

--- src/GroceryMain.java
+++ src/GroceryMain.java
@@ -2,8 +2,13 @@ public class GroceryMain{
 
     public static void main(String[] args){
         GroceryList list = new GroceryList();
+        GroceryList2 list2 = new GroceryList2();
 
-        new GroceryItemOrder("cheese", 2, 23.99, list);
+        FileHandler fh = new FileHandler();
+
+        fh.loadData("groceries.txt", list2);
+
+      /*  new GroceryItemOrder("cheese", 2, 23.99, list);
         new GroceryItemOrder("beer", 6, 20, list);
         new GroceryItemOrder("spinach", 1, 15.20, list);
         new GroceryItemOrder("cheese", 2, 23.99, list);
@@ -13,9 +18,9 @@ public class GroceryMain{
         new GroceryItemOrder("beer", 6, 20, list);
         new GroceryItemOrder("spinach", 1, 15.99, list);
         new GroceryItemOrder("cheese", 2, 23.99, list);
-        new GroceryItemOrder("spinach", 1, 15.20, list);
+        new GroceryItemOrder("spinach", 1, 15.20, list);*/
 
         System.out.println("This is the grocery list:");
-        list.displayList();
+        list2.displayList();
     }
 }
